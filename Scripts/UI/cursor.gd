9d52b333a37ecae8cfe0ff87b1376cf801e1e994
Aaron Van den Bossche <150597661+Bongo6@users.noreply.github.com>
2025-11-23 00:31:23 +0100

    small changes to cursor and visibility

--- Scripts/UI/cursor.gd
+++ Scripts/UI/cursor.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
-@onready var root = $".."
+@onready var menu_actions = $"../ui/menu_actions"
+@onready var cursor = $Sprite2D
+
+@export var texture_1: Texture2D = preload("res://Textures/UI/Cursor/Cursor_1.png")
+@export var texture_2: Texture2D = preload("res://Textures/UI/Cursor/Cursor_2.png")
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	cursor.texture = texture_1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position = get_global_mouse_position()
+	
+	if menu_actions.selected_troop != null:
+		if menu_actions.selected_troop.is_digging == true:
+			cursor.texture = texture_2
